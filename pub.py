acfbf2577f2f4d79bf22ad970969a95d0ee71cfe
Ahmet Pehlivanoğlu <77580131+pehlivanoglu@users.noreply.github.com>
2022-01-23 15:19:12 +0300

    Update pub.py

--- pub.py
+++ pub.py
@@ -35,30 +35,24 @@ if __name__ == "__main__":
 
     figure, axis = plt.subplots(2, 4)
   
-    # Left Vel
     axis[0, 0].plot(motor_data_time, left_vel)
     axis[0, 0].set_title("Left vel")
     
-    # Left Vol
     axis[0, 1].plot(motor_data_time, left_vol)
     axis[0, 1].set_title("Left vol")
-    
-    # For Tangent Function
+   
     axis[0, 2].plot(motor_data_time, left_temp)
     axis[0, 2].set_title("Left temp")
 
     axis[0, 3].plot(motor_data_time, left_curr)
     axis[0, 3].set_title("Left curr")
     
-    # For Tanh Function
     axis[1, 0].plot(motor_data_time, right_vel)
     axis[1, 0].set_title("Right vel")
     
-    # Left Vol
     axis[1, 1].plot(motor_data_time, right_vol)
     axis[1, 1].set_title("Right vol")
     
-    # For Tangent Function
     axis[1, 2].plot(motor_data_time, right_temp)
     axis[1, 2].set_title("Right temp")
 
